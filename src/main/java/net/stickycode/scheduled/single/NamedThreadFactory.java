c706e6b67822a3735ee27e9eb336575fe34cc548
Michael McCallum <michael@StickyCode.net>
2011-08-26 23:20:01 +1200

    Scheduling test works now in hello world ws deploy

--- src/main/java/net/stickycode/scheduled/single/NamedThreadFactory.java
+++ src/main/java/net/stickycode/scheduled/single/NamedThreadFactory.java
@@ -10,7 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package net.stickycode.scheduled;
+package net.stickycode.scheduled.single;
 
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
