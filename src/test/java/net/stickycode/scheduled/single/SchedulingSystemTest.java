6c4c52d746e2bec1c33a8741953409c6af99a9a1
Michael McCallum <michael@StickyCode.net>
2015-08-24 23:44:59 +1200

    Ok I'm sold on jmockit, its been great but I have to say goodbye now Mockito. Java8, assertj.

--- src/test/java/net/stickycode/scheduled/single/SchedulingSystemTest.java
+++ src/test/java/net/stickycode/scheduled/single/SchedulingSystemTest.java
@@ -12,7 +12,7 @@
  */
 package net.stickycode.scheduled.single;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.Iterator;
